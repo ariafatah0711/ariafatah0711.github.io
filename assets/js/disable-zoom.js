76837ab858922ae4a3309a6062126e1c762b88df
ariafatah0711 <ariafatah0711@gmail.com>
2025-02-03 06:19:19 +0700

    update

---
+++ assets/js/disable-zoom.js
@@ -0,0 +1,13 @@
+document.addEventListener(
+  "wheel",
+  function (event) {
+    if (event.ctrlKey) {
+      event.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
+document.addEventListener("gesturestart", function (event) {
+  event.preventDefault();
+});
