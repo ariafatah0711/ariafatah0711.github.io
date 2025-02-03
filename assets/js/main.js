69adc2738451dd10d25c14e5d9589c1b72c6fa8b
ariafatah0711 <ariafatah0711@gmail.com>
2025-02-04 06:50:46 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,15 @@ function toggleNightMode() {
     document.documentElement.setAttribute("data-theme", "dark");
 
     document.getElementById("mode-switcher").classList.add("active");
-    document.getElementById("mode-switcher-info").classList.add("active");
+    // document.getElementById("mode-switcher-info").classList.add("active");
+    document.getElementById("mode-switcher-info")?.classList.add("active");
     localStorage.setItem("theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
 
     document.getElementById("mode-switcher").classList.remove("active");
-    document.getElementById("mode-switcher-info").classList.remove("active");
+    // document.getElementById("mode-switcher-info").classList.remove("active");
+    document.getElementById("mode-switcher-info")?.classList.remove("active");
     localStorage.setItem("theme", "");
   }
 }
