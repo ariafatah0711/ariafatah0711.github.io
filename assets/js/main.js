ba7c0160c31628ab039af7fa5e2147ea4134072f
ariafatah0711 <ariafatah0711@gmail.com>
2025-02-03 19:11:06 +0700

    feature layout song and theme toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 function toggleNightMode() {
   if (document.documentElement.getAttribute("data-theme") == "light") {
     document.documentElement.setAttribute("data-theme", "dark");
+
     document.getElementById("mode-switcher").classList.add("active");
+    document.getElementById("mode-switcher-info").classList.add("active");
     localStorage.setItem("theme", "dark");
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+
     document.getElementById("mode-switcher").classList.remove("active");
+    document.getElementById("mode-switcher-info").classList.remove("active");
     localStorage.setItem("theme", "");
   }
 }
